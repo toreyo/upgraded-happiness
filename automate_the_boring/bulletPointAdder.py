2438ec095db4d6e42c01ad0c51766e3da8587328
toreyo <t.i.oishi@gmail.com>
2020-03-01 20:31:22 -1000

    chapter6projecto

---
+++ automate_the_boring/bulletPointAdder.py
@@ -0,0 +1,17 @@
+#! python3
+# bulletPointAdder.py adds Wikipedia bullet points to start
+# of each line of text on the clipboard
+
+import pyperclip
+text = pyperclip.paste
+
+# TODO: Separate lines and add stars
+
+lines = text.split('\n')
+for i in range(len(lines)): #loop through all indexes in the "lines" list
+    lines[i] = '*' + lines[i] #add start to each string in "lines" list
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
+
+
