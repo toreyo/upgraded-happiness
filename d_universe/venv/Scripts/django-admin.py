2b1f5e1158daf97f9206d078ca950d1aff6cd06e
toreyo <t.i.oishi@gmail.com>
2020-05-01 06:13:24 -1000

    7 hour bat file les go

---
+++ d_universe/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\toreyo\Desktop\dance_universe\d_universe\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
